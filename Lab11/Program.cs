e4ea87d21a1df92b8ca04a7e72ae0d80073f3111
2299112@gmail.com <2299112@gmail.com>
2021-11-27 22:19:11 +0500

    Добавьте файлы проекта.

---
+++ Lab11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Решение уравнения с одной переменной
+            Console.WriteLine("Программа решения уравнения с одной переменной.\n\nВведите значения k и b в уравнении типа: 0=kx+b");
+            Console.Write("k - ");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nb - ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            UravnenieOdnaPeremennaya uravnenieOdnaPeremennaya = new UravnenieOdnaPeremennaya { k = k, b = b };
+            uravnenieOdnaPeremennaya.Root();
+            Console.ReadKey();
+        }
+        struct UravnenieOdnaPeremennaya
+        {
+            public double k;
+            public double b;
+            public void Root()
+            {
+                double x = -b / k;
+                Console.WriteLine("\nx равняется: {0}", x);
+            }
+        }
+    }
+}
